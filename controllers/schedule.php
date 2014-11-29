f82c8357d8a862325cf06af41f23762ad591ca8a
Đào Duy Nhẫn <DaoDuyNhan1993.21@gmail.com>
2014-11-30 04:58:27 +0700

    Đổi tên + gắn thêm thời khoá biểu + cắm php để hiển thị thời khoá biểu+ cắm thêm schedule.

---
+++ controllers/schedule.php
@@ -0,0 +1,5 @@
+<?php
+session_start();
+
+include $_SESSION['BASE_PATH']."/schedule.html";
+?>
---
+++ controllers/timetable.php
@@ -0,0 +1,5 @@
+<?php
+session_start();
+include $_SESSION['BASE_PATH']."/models/user/model_user.php";
+include $_SESSION['BASE_PATH']."/timetable.html";
+?>
